329c631576965681d4d0659a7af6d22346351c90
Edwin Pérez Marulanda <edwinperez@Mac.bbrouter>
2026-02-08 19:27:10 -0500

    Enable CORS globally

---
+++ src/main/java/com/teccah/asistenciaapi/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.teccah.asistenciaapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+
+        // Permitir cualquier origen (para pruebas)
+        config.addAllowedOriginPattern("*");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
